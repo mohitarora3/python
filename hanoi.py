3b96a91d4c3763f952943d5abf41499c657c9708
deepakmca17du <31646033+deepakmca17du@users.noreply.github.com>
2017-11-03 12:34:45 +0530

    Create hanoi.py
    tower of hanoi: transfers the discs from source stack to the destination stack

---
+++ hanoi.py
@@ -0,0 +1,22 @@
+def hanoi(n,source,spare,target):
+    '''
+    objective: to transfer discs from source to target with the heavier disc
+               below the lighter one 
+    input parameters:
+               n: number of discs
+               source: tower from which disc need to be transferred
+               spare: tower which is used as an intermediate to transfer disc
+                      from source to destination
+               target: tower to which the discs needs to be transferred
+    return value: none
+    '''
+
+    assert n>0
+    if n==1:
+        print('Move a disc from ',source,' to ',target)
+    else:
+        hanoi(n-1,source,target,spare)
+        print('Move a disc from ',source,' to ',target)
+        hanoi(n-1,spare,source,target)
+
+hanoi(3,1,2,3)
